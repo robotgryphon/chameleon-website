f0398d9a7f48996397438cc9d28ed83ddd920c2a
Ted <code@robotgryphon.com>
2017-12-07 12:31:45 -0500

    Finished login, implemented basics of redux for state management

---
+++ client/src/chameleon-state.js
@@ -0,0 +1,31 @@
+import { createStore } from 'redux';
+import PolymerRedux from 'polymer-redux';
+
+let defaultState = {
+    user: {
+        uid: '',
+        displayName: 'Guest',
+        isAdmin: false
+    }
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'AUTH_CHANGED':
+            // ALWAYS return new state
+            return Object.assign({}, state, {
+                user: action.user // New message
+            });
+
+        // Return the state, nothing changed.
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer, defaultState);
+
+// Create the PolymerRedux mixin
+const ReduxMixin = PolymerRedux(store);
+
+export default ReduxMixin;

1295d118c2e0bdf56cdcd9ad329409e59dae3ac8
Ted <code@robotgryphon.com>
2017-12-09 05:10:53 -0500

    Some more changes on firebase stuff, fixes the footer not working properly

--- client/src/scripts/chameleon-state.js
+++ client/src/scripts/chameleon-state.js
@@ -1,13 +1,15 @@
 import { createStore } from 'redux';
 import PolymerRedux from './polymer-redux-fork';
 
+let guestState = {
+    uid: '',
+    displayName: 'Guest',
+    isAdmin: false,
+    isLoggedIn: false
+};
+
 let defaultState = {
-    user: {
-        uid: '',
-        displayName: 'Guest',
-        isAdmin: false,
-        isLoggedIn: false
-    }
+    user: guestState
 };
 
 const reducer = (state, action) => {
@@ -17,6 +19,10 @@ const reducer = (state, action) => {
             return Object.assign({}, state, {
                 user: action.user // New message
             });
+
+        case 'USER_LOGGED_OUT':
+            console.log("user logout event");
+            return Object.assign({}, state, { user: guestState });
 
         // Return the state, nothing changed.
         default:

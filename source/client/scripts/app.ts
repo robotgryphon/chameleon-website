f0f5d397f38375df1bf93a6fab4df23348701e4b
Ted Senft <code@robotgryphon.com>
2018-04-15 18:31:47 -0400

    Responsive Footer; Routing.

--- source/client/scripts/app.ts
+++ source/client/scripts/app.ts
@@ -1,1 +1,24 @@
 import "./chameleon-banner";
+
+//@ts-ignore
+import * as Navigo from "navigo";
+
+let router = new Navigo(null, true, "#");
+
+router.on(() => loadPage("home"));
+router.notFound((q: any) => loadPage("not-found", q));
+
+router.resolve();
+
+async function loadPage(page: string, query?: any): Promise<void> {
+    try {
+        let pageFetch: Response = await fetch(`../pages/${page}.html`);
+        let pageData = await pageFetch.text();
+        let pageContainer = document.querySelector("main");
+        pageContainer.innerHTML = pageData;
+    }
+
+    catch {
+        return Promise.reject("Could not load page.");
+    }
+}

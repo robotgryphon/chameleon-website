6a9c01059b65c991ada74e2df0810cb39445b8f8
Ted Senft <code@robotgryphon.com>
2018-04-15 00:15:58 -0400

    Build system complete; Banner.

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,10 +1,34 @@
 import { src, dest, task, watch, series } from "gulp";
 import { createProject } from "gulp-typescript";
 import * as sass from "gulp-sass";
+import * as babel from "gulp-babel";
+import * as webpack from "webpack-stream";
+import * as wp from "webpack";
+
+import { Configuration } from "webpack";
+
+let wpConfig: Configuration = {
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    
+    output: {
+        filename: "app.js"
+    },
+
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: "ts-loader" }
+        ]
+    }
+};
 
 let ts = createProject("tsconfig.json");
+let clientTS = createProject("tsconfig.json", {
+    target: "es6"
+});
 
-task("build:pages", () => {
+task("build:html", () => {
     return src("source/client/**/*.html")
         .pipe(dest("build/client"));
 });
@@ -15,16 +39,27 @@ task("build:styles", () => {
         .pipe(dest("build/client/styles"));
 });
 
+task("build:fonts", () => {
+    return src("source/client/fonts/**/*")
+        .pipe(dest("build/client/fonts"));
+});
+
 task("build:media", () => {
     return src("source/client/media/**/*")
         .pipe(dest("build/client/media"));
 });
 
-task("client", series("build:pages", "build:styles", "build:media"));
+task("build:scripts", () => {
+    return src("source/client/scripts/**/*")
+        .pipe(webpack(wpConfig, wp))
+        .pipe(dest("build/client/scripts"));
+});
+
+task("client", series("build:html", "build:styles", "build:fonts", "build:media"));
 
 // Watch Tasks
-task("watch:pages", () => {
-    let pages = task("build:pages");
+task("watch:html", () => {
+    let pages = task("build:html");
     watch("source/client/**/*.html", pages);
 });
 
